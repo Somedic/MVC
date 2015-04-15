3ae2bbbb5c421928847f727fdd82944a8e1332b3
Somedic <somedic@hotmail.ch>
2015-04-15 19:46:25 +0200

    Ajout d'information dans la $_SESSION sur les propriété users afin de les injectés dans la session.

--- core/Auth/DBAuth.php
+++ core/Auth/DBAuth.php
@@ -25,9 +25,11 @@ class DBAuth{
         $user = $this->db->prepare('SELECT * FROM users WHERE login = ?',[$username], null, true);
         if($user){
          if($user->password === sha1($password)){
-             $_SESSION['auth'] = $user->id;
-             $_SESSION['email'] = $user->email;
+             $_SESSION['auth']   = $user->id;
+             $_SESSION['email']  = $user->email;
              $_SESSION['pseudo'] = $user->pseudo;
+             $_SESSION['user_id']= $user->id;
+             $_SESSION['role']   = $user->role;
              return true;
          }
         }
